32af38bca4cad484051bd45847c34ab0cd8598ba
Tsitohaina Sandatia <hei.sandatia@gmail.com>
2026-08-27 15:55:30 +0100

    Fix/fix routes app.jsx (#7)
    * Feat/configuration routes app.jsx (#3)
    
    * feat: new start without TypeScript
    
    * feat: add the login page
    
    * chore: add pnpm dependencies
    
    * chore: refactor README
    
    * feat: add all routes of App.jsx
    
    * feat: configuration of route in App.jsx
    
    ---------
    
    Co-authored-by: Venusaur111 <hei.herisoa@gmail.com>
    
    * fix: correction of all routes in App.jsx
    
    * feat: add all api
    
    * fix: fix routes in App.jsx
    
    ---------
    
    Co-authored-by: Venusaur111 <hei.herisoa@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,88 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/login/LoginPage';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import AdminStudentList from './pages/admin/AdminStudentList';
-import AdminCourses from './pages/admin/AdminCourses';
-import AdminExam from './pages/admin/AdminExam';
-import StudentExamList from './pages/students/StudentExamList';
-import StudentResults from './pages/students/StudentResults';
+import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import LoginPage from "./pages/login/LoginPage";
+
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminStudentList from "./pages/admin/AdminStudentList";
+import AdminCourses from "./pages/admin/AdminCourses";
+import AdminExam from "./pages/admin/AdminExam";
+import EditionQuestion from "./pages/admin/EditionQuestion";
+import ExamResults from "./pages/admin/ExamResults";
+
+import StudentExamList from "./pages/students/StudentExamList";
+import StudentExamPage from "./pages/students/StudentExamPage";
+import StudentResultPage from "./pages/students/StudentResultPage";
+import StudentResults from "./pages/students/StudentResults";
+
+import ProtectedRoute from "./components/common/ProtectedRoute";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/api/v1' element={<LoginPage/>}/>
-          <Route path=''/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
 
-          <Route path='/admin' element={<AdminDashboard/>}/>
-          <Route path='/admin/student' element={<AdminStudentList/>}/>
-          <Route path='/admin/courses' element={<AdminCourses/>}/>
-          <Route path='/admin/exams' element={<AdminExam/>}/>
-          <Route path='/admin/exams/:id/questions' element={<></>}/>
-          <Route path='/admin/exams/:id/results' element={<></>}/>
+        <Route element={<ProtectedRoute role="admin" />}>
+          <Route
+            path="/admin"
+            element={<AdminDashboard />}
+          />
 
-          <Route path='/student' element={<StudentExamList/>}/>
-          <Route path='/student/exams/:id' element={<></>}/>
-          <Route path='/student/exams/:id/result' element={<></>}/>
-          <Route path='/student/results' element={<StudentResults/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+          <Route
+            path="/admin/students"
+            element={<AdminStudentList />}
+          />
+
+          <Route
+            path="/admin/courses"
+            element={<AdminCourses />}
+          />
+
+          <Route
+            path="/admin/exams"
+            element={<AdminExam />}
+          />
+
+          <Route
+            path="/admin/exams/:id/questions"
+            element={<EditionQuestion />}
+          />
+
+          <Route
+            path="/admin/exams/:id/results"
+            element={<ExamResults />}
+          />
+        </Route>
+
+        <Route element={<ProtectedRoute role="student" />}>
+          <Route
+            path="/student"
+            element={<StudentExamList />}
+          />
+
+          <Route
+            path="/student/exams/:id"
+            element={<StudentExamPage />}
+          />
+
+          <Route
+            path="/student/exams/:id/result"
+            element={<StudentResultPage />}
+          />
+
+          <Route
+            path="/student/results"
+            element={<StudentResults />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
