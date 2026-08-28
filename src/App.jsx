82d5a6bcde30680dbd5d2a9529693cabd5906516
tsitohainassandatia <hei.sandatia@gmail.com>
2026-08-28 12:46:56 +0100

    feat: re-add the student page for admin

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
 import Login from "./pages/login/Login";
 import Dashboard from "./pages/admin/Dashboard";
+import StudentPage from "./pages/admin/StudentPage";
 
 
 
@@ -35,7 +36,7 @@ const App = ()=> {
             <Route element={<ProtectedRoute role="admin" />}>
               <Route element={<Layout />}>
                 <Route path="/admin" element={<Dashboard/>}/>
-                <Route path="/admin/students"/>
+                <Route path="/admin/students" element={<StudentPage/>}/>
                 <Route path="/admin/courses"/>
                 <Route path="/admin/exams"/> {/**route for exam lists */}
                 <Route path="/admin/exams/:id/questions"/> {/**route for seing a specific exam */}
