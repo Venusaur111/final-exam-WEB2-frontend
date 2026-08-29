b3b4e49b6ceea9e6c7d320ebcef91d9258cb1bda
A.H RAKOTOARINJATO <hei.herisoa@gmail.com>
2026-08-29 03:45:25 +0300

    fix: re-fix the routes in App.jsx (#13)
    Co-authored-by: tsitohainassandatia <hei.sandatia@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Layout from "./components/Layout";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider, useAuth } from "./context/AuthContext";
-import { BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/login/Login";
 import Dashboard from "./pages/admin/Dashboard";
@@ -13,11 +13,13 @@ import EditionQuestion from "./pages/admin/EditionQuestion";
 import ExamList from "./pages/student/ExamList";
 import ResultList from "./pages/student/ResultList";
 import ExamStudent from "./pages/student/ExamStudent";
+import ExamResults from "./pages/admin/ExamResult";
+import ResultStudent from "./pages/student/ResultStudent";
 
 
 
 
-const HomeRedirect = ()=>{
+const HomeRedirect = () => {
   const { isAuthenticated, user } = useAuth();
 
   if (!isAuthenticated) {
@@ -32,32 +34,32 @@ const HomeRedirect = ()=>{
   );
 };
 
-const App = ()=> {
-  return(
+const App = () => {
+  return (
     <>
       <AuthProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/login" element={<Login />}/>
-            <Route path="*" element={<HomeRedirect />}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<HomeRedirect />} />
 
             <Route element={<ProtectedRoute role="admin" />}>
               <Route element={<Layout />}>
-                <Route path="/admin" element={<Dashboard/>}/>
-                <Route path="/admin/students" element={<StudentPage/>}/>
-                <Route path="/admin/courses" element={<CoursePage/>}/>
-                <Route path="/admin/exams" element={<ExamPage/>}/> {/**route for exam lists */}
-                <Route path="/admin/exams/:id/questions" element={<EditionQuestion/>}/> {/**route for seing a specific exam */}
-                <Route path="/admin/exams/:id/results"/> 
+                <Route path="/admin" element={<Dashboard />} />
+                <Route path="/admin/students" element={<StudentPage />} />
+                <Route path="/admin/courses" element={<CoursePage />} />
+                <Route path="/admin/exams" element={<ExamPage />} /> {/**route for exam lists */}
+                <Route path="/admin/exams/:id/questions" element={<EditionQuestion />} /> {/**route for seing a specific exam */}
+                <Route path="/admin/exams/:id/results" element={<ExamResults />} />
               </Route>
             </Route>
 
-            <Route element={<ProtectedRoute role="student"/>}>
+            <Route element={<ProtectedRoute role="student" />}>
               <Route element={<Layout />}>
-                <Route path="/student" element={<ExamList/>}/>
-                <Route path="/student/results" element={<ResultList/>}/>
-                <Route path="/student/exams/:id" element={<ExamStudent/>}/> {/**route when the student want to pass an exam */}
-                <Route path="/student/exams/:id/result" element={<></>}/> 
+                <Route path="/student" element={<ExamList />} />
+                <Route path="/student/results" element={<ResultList />} />
+                <Route path="/student/exams/:id" element={<ExamStudent />} /> {/**route when the student want to pass an exam */}
+                <Route path="/student/exams/:id/result" element={<ResultStudent />} />
               </Route>
             </Route>
           </Routes>
@@ -65,7 +67,7 @@ const App = ()=> {
       </AuthProvider>
     </>
   );
-  
+
 }
 
 export default App;
